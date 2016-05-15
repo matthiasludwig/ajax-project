c41db1526501373854dc9c493b9f81fe852d5615
Matthias Ludwig <matthiasludwig@me.com>
2016-05-15 23:58:01 +0000

    Added Google Streetview Background Image

--- js/script.js
+++ js/script.js
@@ -12,8 +12,35 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetName = $('#street').val();
+    var cityName = $('#city').val();
+    var addressName = streetName + ', ' + cityName;
+    var googleImg = "https://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + streetName + ', ' + cityName;
+    $body.append('<img class="bgimg" src="' + googleImg + '">');
 
-    // YOUR CODE GOES HERE!
+    $greeting.text("So you want to live at " + addressName);
+
+    //load NYT
+
+    // Built by LucyBot. www.lucybot.com
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "ac16f8df9a34441fa322642dbb19ed02",
+      'q' : cityName
+    });
+
+    $.getJSON( url, function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+
+    $( $nytHeaderElem, {
+        "class": ".article",
+        html: items.join( "" )
+        }).appendTo( $nytHeaderElem );
+    });
+
 
     return false;
 };
